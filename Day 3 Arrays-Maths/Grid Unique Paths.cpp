3a8cc65ca04a5f984aa3153dffc35bd0d3130acb
Jyotika Sharma <37184178+Jyotika25@users.noreply.github.com>
2020-09-03 19:01:45 +0530

    LeetCode 62
    https://leetcode.com/problems/unique-paths/

---
+++ Day 3 Arrays-Maths/Grid Unique Paths.cpp
@@ -0,0 +1,40 @@
+//Approach 1 : Dynamic Programming
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int dp[m][n];
+        
+        for(int i=0;i<m;i++)
+            dp[i][0] = 1;
+        for(int j=0;j<n;j++)
+            dp[0][j] = 1;
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+//Approach 2 : permutation based 
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int N = m+n-2;
+        int R =m-1;
+        double res=1;
+        
+        for(int i=1;i<=R;i++){
+            res = res * (N-R+i)/i;
+        }
+        return int(res);
+    }
+};
